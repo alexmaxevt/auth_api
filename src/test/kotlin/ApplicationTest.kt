28cfacd2723f2d1e22cf28445272cb866c6088fd
Anatoly Spitchenko <anatolij.sp@yandex.ru>
2020-08-20 01:35:04 +0700

    Поправлены тесты

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,8 +1,22 @@
+import com.jayway.jsonpath.JsonPath
+import io.ktor.application.Application
+import io.ktor.config.MapApplicationConfig
+import io.ktor.http.*
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
+import io.ktor.server.testing.withTestApplication
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import ru.evtukhov.ru.evtukhov.module
+
 class ApplicationTest {
     private val jsonContentType = ContentType.Application.Json.withCharset(Charsets.UTF_8)
     private val configure: Application.() -> Unit = {
         (environment.config as MapApplicationConfig).apply {
             put("ncraft.jwt.secret", "secret")
+            put("ncraft.upload.dir", createTempFile().absolutePath)
         }
         module()
     }
@@ -10,7 +24,7 @@ class ApplicationTest {
     @Test
     fun testUnauthorized() {
         withTestApplication(configure) {
-            with(handleRequest(HttpMethod.Get, "/api/v1/me")) {
+            with(handleRequest(HttpMethod.Get, "/api/v1/posts")) {
                 response
                 assertEquals(HttpStatusCode.Unauthorized, response.status())
             }
@@ -39,13 +53,11 @@ class ApplicationTest {
                     token = JsonPath.read<String>(response.content!!, "$.token")
                 }
                 delay(500)
-                with(handleRequest(HttpMethod.Get, "/api/v1/me") {
+                with(handleRequest(HttpMethod.Get, "/api/v1/posts") {
                     addHeader(HttpHeaders.Authorization, "Bearer $token")
                 }) {
                     response
                     assertEquals(HttpStatusCode.OK, response.status())
-                    val username = JsonPath.read<String>(response.content!!, "$.username")
-                    assertEquals("vasya", username)
                 }
             }
         }
